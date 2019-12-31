5f8074ee1f07bda158df60fda54bb2cc4d9991e2
Wesley Juarez <wesleyjuarez7@gmail.com>
2019-12-31 09:10:57 -0500

    ALmost Done
    just can't get popup to showup

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('hey there!')
+
+function showPopup(){
+	var popup = document.getElementById('popup');
+	popup.classList.add('show')
+}
+function hidePopup(){
+	var popup=document.getElementById('popup');
+	popup.classList.remove('show')
+}
